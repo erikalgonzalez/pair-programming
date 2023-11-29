488e55b5508081393171403faee5c2df3ab68e45
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-29 14:20:23 -0500

    created arrays to see if sam and sally should date

--- pair_programming.rb
+++ pair_programming.rb
@@ -27,10 +27,27 @@
 
 # 1. Create and define a variable count = 0. Using a loop and the += operator, output the following:
 
-count = 0
-while count <= 10
-  p count
-  count += 1
+# count = 0
+# while count <= 10
+#   p count
+#   count += 1
+# end 
+
+
+
+# 1. Translate the following into ruby code. Run the program to make sure it works:
+
+# Sam enjoys cooking. Make an array with recipe names to represent the different recipes Sam can cook.
+# Sally speaks many languages. Make an array with language names to represent the languages Sally can speak.
+# If Sam can cook more than 10 recipes and Sally speaks more than 5 languages, they should date. Based on the above arrays, print out a message describing whether or not they should date.
+# If Sam can make crepes or Sally can speak French, they should marry. Based on the above arrays, print out a message describing whether or not they should marry.
+
+
+recipes = ["mac & cheese", "stuffing", "pumpkin pie"]
+languages = ["Spanish", "English", "Italian"]
+
+if recipes.length > 10 && languages.length > 5
+  puts "Sam and Sally should date"
+else
+  puts "Sam and Sally should not date"
 end 
-
-
