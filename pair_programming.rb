ce572785d911d7292b70e248aa985b8e39cb8d89
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-29 13:49:43 -0500

    Created a count 1-10

--- pair_programming.rb
+++ pair_programming.rb
@@ -6,24 +6,31 @@
 # 4.  Swedish meatballs
 # 5.  mac and cheese
 
-puts "What are your 5 favorite foods?"
+# puts "What are your 5 favorite foods?"
 
-foods_list = []
-5.times do 
-  favorite_food = gets.chomp
-  foods_list << favorite_food
-end
+# foods_list = []
+# 5.times do 
+#   favorite_food = gets.chomp
+#   foods_list << favorite_food
+# end
 
-p foods_list
+# p foods_list
 
-i = 0
-count = 1
-while i < foods_list.length
-  food = foods_list[i]
-  puts "#{count}. #{food}"
-  i += 1
+# i = 0
+# count = 1
+# while i < foods_list.length
+#   food = foods_list[i]
+#   puts "#{count}. #{food}"
+#   i += 1
+#   count += 1
+# end 
+
+# 1. Create and define a variable count = 0. Using a loop and the += operator, output the following:
+
+count = 0
+while count <= 10
+  p count
   count += 1
 end 
-
 
 
