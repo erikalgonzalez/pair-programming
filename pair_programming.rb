03c1e4b55b2b43c33e02159e001094320ac16baf
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-29 13:46:40 -0500

    Changed favorite foods array into an ordered list

---
+++ pair_programming.rb
@@ -0,0 +1,29 @@
+# 3. Finally, change your program so that when the list is printed in the terminal, each food is prefaced with a number, beginning with 1 and going up to 5, for example:
+
+# 1.  cobb salad
+# 2.  spaghetti
+# 3.  pizza
+# 4.  Swedish meatballs
+# 5.  mac and cheese
+
+puts "What are your 5 favorite foods?"
+
+foods_list = []
+5.times do 
+  favorite_food = gets.chomp
+  foods_list << favorite_food
+end
+
+p foods_list
+
+i = 0
+count = 1
+while i < foods_list.length
+  food = foods_list[i]
+  puts "#{count}. #{food}"
+  i += 1
+  count += 1
+end 
+
+
+
